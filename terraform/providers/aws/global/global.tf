a637b32c1316e65f8b9ac043c4db686144989c16
hiroqn <hiroqn1008@gmail.com>
2016-08-02 18:43:44 +0900

    👻

--- terraform/providers/aws/global/global.tf
+++ terraform/providers/aws/global/global.tf
@@ -12,6 +12,15 @@ provider "aws" {
   region = "${var.region}"
 }
 
+resource "aws_iam_account_password_policy" "strict" {
+  minimum_password_length        = 10
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+}
+
 module "iam_admin" {
   source = "../modules/iam"
 
